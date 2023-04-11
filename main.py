ef135ada19a243fca8b76d4b1f5ea3e67f3ad855
Room 404 <dbuhanov366@gmail.com>
2023-04-11 22:44:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ bot = TeleBot(TOKEN)
 
 @bot.message_handler(content_types=["text"])
 def wright(message):
+    '''Необходима для написания чего-либо для пользователя'''
     bot.send_message(message.chat.id, message.text)
     
     
 # Старт
 bot.polling(none_stop=True)
+
+
+# t.me/liliindexsbot - ссылка на бота
