066f8df80b3a5d7271f2abe7233366c6609e8842
Room 404 <dbuhanov366@gmail.com>
2023-04-11 23:12:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,27 @@ from const import *
 bot = TeleBot(TOKEN)
 
 
-
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    '''Приветствие пользователя '''
+    # Стикер приветствия
+    bot.send_sticker(message.chat.id, open('static//img/lili_hello.png', 'rb'))
+    
+    # Пишем "Добро пожаловать 'имя пользователя'!"
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!".format(message.from_user))
 
 
 @bot.message_handler(content_types=["text"])
 def wright(message):
     '''Необходима для взаимодействия с пользователем'''
+    # message - то что написал пользователь
     # Пишем то, что написал пользователь
     bot.send_message(message.chat.id, message.text)
     
     
     
     
-# Старт
+# Старт бота
 bot.polling(none_stop=True)
 
 
