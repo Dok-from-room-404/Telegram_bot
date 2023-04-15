459f53d903a68cba44071dd4d5742cdba6e1520f
Room 404 <dbuhanov366@gmail.com>
2023-04-15 23:54:12 +0900

    Update const.py

--- const.py
+++ const.py
@@ -8,6 +8,13 @@
 
 TOKEN = "6218093072:AAHR9dUmAo24AcMOmWIl-QIGe3Ln2btGQuA" # Токе бота от BotFather
 
+TEXT_FOR_HELP = '''
+Список доступных команд:
+\t/help - список доступных команд
+\t/start – команда старт
+
+'''
+
 
 class File():
     '''Класс файла который будет обрабатывать бот. 
