d7ccb3d52ad6c4d0849e6e4d3f94e220c8ac38e8
Room 404 <dbuhanov366@gmail.com>
2023-04-16 15:21:02 +0900

    Update __init__.py

--- modle/YouTube/__init__.py
+++ modle/YouTube/__init__.py
@@ -7,7 +7,7 @@ from pytube import YouTube
 
 
 
-class YouTube:
+class File_YouTube:
     def __init__(self, link) -> None:
         # yt = YouTube('https://www.youtube.com/shorts/96LQhbSIFWI')
         self.yt = YouTube(link)
