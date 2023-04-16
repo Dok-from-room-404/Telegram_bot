ec59dd64f891beb432d23f6a07ba430df90a0a25
Room 404 <dbuhanov366@gmail.com>
2023-04-16 14:11:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,26 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from random import choice
 from const import *
+from file import File
+
+
 
 # Экземпляр бота
 bot = Bot(TOKEN)
 # Диспечер бота. Отсеживает сообщения
 dp = Dispatcher(bot)
+# Класс файла который будет обрабатывать бот. Необходим чтобы бот понимал на каком он этапе обработки файла
 file = File()
 
 
 @dp.message_handler(commands=['help'])
-async def help_command(message: types.Message):
+async def help(message: types.Message):
     '''Помощь для пользователя '''
     await message.reply(TEXT_FOR_HELP, reply_markup=ReplyKeyboardRemove())
 
 
-
-
 @dp.message_handler(commands=['start'])
-async def welcome(message: types.Message):
+async def start(message: types.Message):
     '''Приветствие пользователя '''
     # Список стикеров для приветствия пользователя
     sp = ["static//img/lili_hello.png", "static//img/lili.png"]
@@ -37,19 +39,49 @@ async def welcome(message: types.Message):
                      
     # await bot.send_message(message.chat.id, "Я {bot}, могу скачивать видео и аудио".format(bot = bot.get_me().first_name))
     await message.answer("Я могу скачивать видео и аудио из социальный сетей")
+    file.reset()
+    message.text = ""
     await wright(message)
     
     
 
     
 
+@dp.message_handler(commands=['dowload'])
+async def dowload(message: types.Message):
+    '''Перезапуск функции скачки'''
+    file.reset()
+    await wright(message)
 
-@dp.message_handler(content_types=["text"], commands=['dowload'])
-async def wright(message):
+
+
+@dp.message_handler(content_types=["text"])
+async def wright(message: types.Message):
     '''Необходима для взаимодействия с пользователем'''
     # message - то что написал пользователь
     # Пишем то, что написал пользователь
+    print(message.text)
+    if not file.have_net():
+        if message.text != "":
+            try:
+                file.append_net(message.text)
+            except Exception as er:
+                print("error")
+                await message.answer(er)
+                
+        else:
+            # resize_keyboard - адаптация под интерфейс
+            markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=3) # default - False
+            b1 = KeyboardButton('YouTube')
+            b2 = KeyboardButton('TikTok')
+            b3 = KeyboardButton('VK')
+            markup.add(b1, b2, b3)
+            #markup.add(item1).insert(item2).add(item3)
+            await message.answer("Из какой социальной сети будем что-либо скачивать:", reply_markup=markup)
     
+    
+    
+    '''
     if file.set == None:
         "До того как пользователь ввел соц сеть"
         if message.text == "YouTube":
@@ -66,8 +98,7 @@ async def wright(message):
             # Удаляем кнопки
             await message.answer("Введи ссылку: ", reply_markup=ReplyKeyboardRemove())
         else:
-            # resize_keyboard - адаптация полд интерфейс
-            
+            # resize_keyboard - адаптация под интерфейс
             markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=3) # default - False
             b1 = KeyboardButton('YouTube')
             b2 = KeyboardButton('TikTok')
@@ -75,13 +106,13 @@ async def wright(message):
             markup.add(b1, b2, b3)
             #markup.add(item1).insert(item2).add(item3)
             await message.answer("Из какой социальной сети будем что-либо скачивать:", reply_markup=markup)
-    '''
+    
     elif file.set != None:
         "После того как пользователь ввел соц сеть"
-        bot.send_message(message.chat.id, "После того как пользователь ввел соц сеть")
-        ...
+        await message.answer("После того как пользователь ввел соц сеть")'''
+        
     
-    bot.send_message(message.chat.id, message.text)'''
+    '''bot.send_message(message.chat.id, message.text)'''
     
     
 
@@ -91,7 +122,7 @@ async def wright(message):
     
 if __name__ == "__main__":
     # Старт бота
-    executor.start_polling(dp)#, on_startup=on_startup)
+    executor.start_polling(dp)
     
 
 
