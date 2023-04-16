479e02ccac1bc63c66695825d3d86bed6562832d
Room 404 <dbuhanov366@gmail.com>
2023-04-16 22:30:33 +0900

    Update __init__.py

--- modle/YouTube/__init__.py
+++ modle/YouTube/__init__.py
@@ -8,6 +8,16 @@ from pytube import YouTube
 
 
 class File_YouTube:
-    def __init__(self, link) -> None:
-        # yt = YouTube('https://www.youtube.com/shorts/96LQhbSIFWI')
+    '''Данный класс необходим для взаимодействия с YouTube (выкачка файлов)'''
+    def __init__(self, link:str) -> None:
+        '''Инициализируем класс
+        * link - Ссылка на видео'''
+        # видео на YouTube (неопределено по формату и качеству)
         self.yt = YouTube(link)
+        
+    def format(self):
+        '''Определяем формат у видео'''
+        formats = set()
+        for i in self.yt.streams:
+            formats.add(i.mime_type.replace(i.type, "")[1:])
+        print(formats)
