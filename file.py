2957203353b29325221bb1399e3c7e004068b895
Room 404 <dbuhanov366@gmail.com>
2023-04-16 14:11:49 +0900

    Update file.py

--- file.py
+++ file.py
@@ -2,16 +2,66 @@
 
 
 
+'''
+Данный модуль содержит классы и  константы для работы бота с файлами
+
+Константы:
+---------
+    * NET - Список доступных соц сетей
+    
+Исключения:
+----------
+    * NET_ERROR - Класс ошибки. Возникает при добавление недопустимой соц. сети
+    
+Классы:
+------
+    * File - Класс файла который будет обрабатывать бот. Необходим чтобы бот понимал на каком он этапе обработки файла
+'''
+
+
+class NET_ERROR(Exception):
+    '''Класс ошибки. Возникает при добавление недопустимой соц. сети'''
+    def __init__(self, inform="") -> None:
+        self.inform = inform
+        
+    def __str__(self):
+        print('calling str')
+        if self.inform:
+            return 'NET_ERROR: {0} '.format(self.inform)
+        else:
+            return 'NET_ERROR'
+
+
+
+# Список доступных соц сетей
+NET = ["YouTube", "TikTok", "VK"]
+
 class File(object):
     '''Класс файла который будет обрабатывать бот. 
        Необходим чтобы бот понимал на каком он этапе обработки файла'''
    
     def __init__(self) -> None:
         '''Инициализация класса с основными атрибутами'''
-        self.set = None # Социальная сеть
-        self.class_set = None # Класс социальной сети (нужен для скачивания)
+        self.__net = None # Социальная сеть
+        self.set = None
+        self.class_net = None # Класс социальной сети (нужен для скачивания)
+        
+    def have_net(self) -> bool:
+        '''Проверка на наличие социальной сети:
+           \n* True - есть социальная сеть
+           \n* False - нету социальной сети'''
+        return self.__net != None
+    
+    def append_net(self, name_net:str) -> None:
+        '''Добавление социальной сети'''
+        if name_net in NET:
+            self.__net = name_net
+        else:
+            raise NET_ERROR("Выбрана не верная социальная сеть. Выберете из предложенных")
+        
+        
         
     def reset(self) -> None:
         '''Сброс настроек класса'''
-        self.set = None
-        self.class_set = None
+        self.__net = None
+        self.class_net = None
