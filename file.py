f015214f85a842bb5f3a7d2dc6f6c711d8342a28
Room 404 <dbuhanov366@gmail.com>
2023-04-16 15:11:35 +0900

    Update file.py

--- file.py
+++ file.py
@@ -12,39 +12,50 @@
 Исключения:
 ----------
     * NET_ERROR - Класс ошибки. Возникает при добавление недопустимой соц. сети
+    * LINK_ERROR - Класс ошибки. Возникает при добавление недопустимой ссылки
     
 Классы:
 ------
     * File - Класс файла который будет обрабатывать бот. Необходим чтобы бот понимал на каком он этапе обработки файла
 '''
+# Список доступных соц сетей
+NET = ["YouTube", "TikTok", "VK"]
 
 
 class NET_ERROR(Exception):
     '''Класс ошибки. Возникает при добавление недопустимой соц. сети'''
     def __init__(self, inform="") -> None:
         self.inform = inform
-        
+
     def __str__(self):
         print('calling str')
         if self.inform:
             return 'NET_ERROR: {0} '.format(self.inform)
         else:
             return 'NET_ERROR'
+        
+        
+class LINK_ERROR(Exception):
+    '''Класс ошибки. Возникает при добавление недопустимой ссылки'''
+    def __init__(self, inform="") -> None:
+        self.inform = inform
 
+    def __str__(self):
+        print('calling str')
+        if self.inform:
+            return 'LINK_ERROR: {0} '.format(self.inform)
+        else:
+            return 'LINK_ERROR'
 
-
-# Список доступных соц сетей
-NET = ["YouTube", "TikTok", "VK"]
 
 class File(object):
     '''Класс файла который будет обрабатывать бот. 
        Необходим чтобы бот понимал на каком он этапе обработки файла'''
-   
+
     def __init__(self) -> None:
         '''Инициализация класса с основными атрибутами'''
         self.__net = None # Социальная сеть
-        self.set = None
-        self.class_net = None # Класс социальной сети (нужен для скачивания)
+        self.__class_net = None # Класс социальной сети (нужен для скачивания)
         
     def have_net(self) -> bool:
         '''Проверка на наличие социальной сети:
@@ -59,9 +70,25 @@ class File(object):
         else:
             raise NET_ERROR("Выбрана не верная социальная сеть. Выберете из предложенных")
         
+    def append_link(self, link:str):
+        '''Добавление ссылки'''
+        # Проверка на правильное начало ссылки
+        if not link.startswith("https://www") and not (self.__net == "VK" and link.startswith("https://")):
+            raise LINK_ERROR("Введена не допустимая ссылка 1")
+        
+        if self.__net == "YouTube" and "youtube.com" not in link:
+            raise LINK_ERROR("Введена не допустимая ссылка 2")
+        
+        elif self.__net == "TikTok" and "tiktok.com" not in link:
+            raise LINK_ERROR("Введена не допустимая ссылка 3")
+        
+        elif self.__net == "VK" and "vk.com" not in link:
+            raise LINK_ERROR("Введена не допустимая ссылка 4")
         
         
+    
+
     def reset(self) -> None:
         '''Сброс настроек класса'''
         self.__net = None
-        self.class_net = None
+        self.__class_net = None
