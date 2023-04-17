0547229c3a447cd114f1fe01d3d29a4d959c976d
Room 404 <dbuhanov366@gmail.com>
2023-04-17 23:17:42 +0900

    Update __init__.py

--- modle/User/__init__.py
+++ modle/User/__init__.py
@@ -41,11 +41,17 @@ class USER:
         print("state 2")
         return self.file.stage == 2 # self.file.check_class_net() and self.file.stage == 2
     
+    def sheck_stage_question_format(self) -> bool: 
+        '''Проверка на стадию question_format (Данный формат является аудио дорожкой. Согласны ли вы изменить формат (да/нет)?):
+        \t* True - текущая стадия question_format
+        \t* False - текущая стадия question_format'''
+        print("state question_format")
+        return self.file.stage == "question_format"
+    
     def sheck_stage_3(self) -> bool: 
         '''Проверка на стадию 3 (Ввод типа файла (видео/аудио)):
         \t* True - текущая стадия 3
         \t* False - текущая стадия 3'''
         print("state 3")
-        return self.file.stage == 3
-        
+        return self.file.stage == 3  
         
