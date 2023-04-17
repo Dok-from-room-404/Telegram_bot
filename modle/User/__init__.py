0fe97d428676bd7479e89ec35252497bab3de4d1
Room 404 <dbuhanov366@gmail.com>
2023-04-17 21:28:46 +0900

    Update __init__.py

--- modle/User/__init__.py
+++ modle/User/__init__.py
@@ -2,50 +2,50 @@
 
 
 
-from .file import File
+'''
+Данная библиотека позволяет работать и с пользователями
+'''
+
+from .Files import File
+from .Files import NET_ERROR, LINK_ERROR
+from .command_bd import *
 
 
 class USER:
     '''Класс пользователя'''
     def __init__(self) -> None:
         '''Инициализация класса пользователя с основными атрибутами:
-        \n* self.file - ласс файла который будет обрабатывать бот. Необходим чтобы бот понимал на каком он этапе обработки файла'''
+        \n* self.file - класс файла который будет обрабатывать бот. Необходим чтобы бот понимал на каком он этапе обработки файла'''
         # Класс файла который будет обрабатывать бот. Необходим чтобы бот понимал на каком он этапе обработки файла
         self.file = File()
-        
-    def append_net(self, net:str):
-        '''Добавление социальной сети'''
-        self.file.append_net(net)
-        
-    def append_link(self, link:str):
-        '''Добавление ссылки и класса социальной сети'''
-        self.file.append_link(link)
-        
-    def get_format(self) -> set:
-        '''Возвращает возможные форматы файлов'''
-        return self.file.get_format()
         
 
     def sheck_stage_0(self) -> bool: 
         '''Проверка на стадию 0 (Выбор соц. сети):
         \t* True - текущая стадия 0
         \t* False - текущая стадия 0'''
-        return not self.file.check_net() and self.file.stage == 0
+        print("state 0")
+        return self.file.stage == 0 # not self.file.check_net() and 
     
     def sheck_stage_1(self) -> bool: 
         '''Проверка на стадию 1 (Ввод ссылки):
         \t* True - текущая стадия 1
         \t* False - текущая стадия 1'''
-        return self.file.check_net() and self.file.stage == 1
+        print("state 1")
+        return self.file.stage == 1 # self.file.check_net() and self.file.stage == 1
     
     def sheck_stage_2(self) -> bool: 
-        '''Проверка на стадию 2 (взаимодействие с классом соц. сети):
+        '''Проверка на стадию 2 (Ввод формата файла):
         \t* True - текущая стадия 2
         \t* False - текущая стадия 2'''
         print("state 2")
-        return self.file.check_class_net() and self.file.stage == 2
+        return self.file.stage == 2 # self.file.check_class_net() and self.file.stage == 2
+    
+    def sheck_stage_3(self) -> bool: 
+        '''Проверка на стадию 3 (Ввод типа файла (видео/аудио)):
+        \t* True - текущая стадия 3
+        \t* False - текущая стадия 3'''
+        print("state 3")
+        return self.file.stage == 3
         
         
-    def reset_file(self) -> None:
-        '''Сброс настроек класса файла'''
-        self.file.reset()
