28da8f6b6dd0ae379058c9741a7e9ca9bad69fb0
Room 404 <dbuhanov366@gmail.com>
2023-04-17 20:31:14 +0900

    Create errors.py

---
+++ modle/User/Files/errors.py
@@ -0,0 +1,39 @@
+
+
+
+
+'''
+Данный модуль содержит исключения для работы бота с файлами
+
+Исключения:
+----------
+    * NET_ERROR - Класс ошибки. Возникает при добавление недопустимой соц. сети
+    * LINK_ERROR - Класс ошибки. Возникает при добавление недопустимой ссылки
+'''
+
+
+
+class NET_ERROR(Exception):
+    '''Класс ошибки. Возникает при добавление недопустимой соц. сети'''
+    def __init__(self, inform="") -> None:
+        self.inform = inform
+
+    def __str__(self):
+        print('calling str')
+        if self.inform:
+            return 'NET_ERROR: {0} '.format(self.inform)
+        else:
+            return 'NET_ERROR'
+
+
+class LINK_ERROR(Exception):
+    '''Класс ошибки. Возникает при добавление недопустимой ссылки'''
+    def __init__(self, inform="") -> None:
+        self.inform = inform
+
+    def __str__(self):
+        print('calling str')
+        if self.inform:
+            return 'LINK_ERROR: {0} '.format(self.inform)
+        else:
+            return 'LINK_ERROR'
