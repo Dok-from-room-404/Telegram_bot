c2f8d6f5891435b5a447650ea1864932fc92f189
Room 404 <dbuhanov366@gmail.com>
2023-04-17 22:25:03 +0900

    Update __init__.py

--- modle/YouTube/__init__.py
+++ modle/YouTube/__init__.py
@@ -14,8 +14,20 @@ class File_YouTube:
         # видео на YouTube (неопределено по формату и качеству)
         self.yt = YouTube(link)
         self.formats = set()
+        self.inform = None
         
     def format(self) -> set:
         '''Определяем формат у видео'''
-        self.formats = set(map(lambda i: i.mime_type.replace(i.type, "")[1:], self.yt.streams))
+        if len(self.formats) == 0:
+            self.formats = set(map(lambda i: i.mime_type.replace(i.type, "")[1:], self.yt.streams))
         return self.formats
+    
+    def set_format(self, format:str) -> bool:
+        '''Устанавливает формат format скачиваемому файлу
+            \n* True - выбранный формат не находится в списке допустимых 
+            \n* False - выбранный формат находится в списке допустимых'''
+        if format not in self.formats:
+            return True
+        # Отсекаем не нужные форматы
+        self.inform = self.yt.streams.filter(file_extension=format)
+        return False
