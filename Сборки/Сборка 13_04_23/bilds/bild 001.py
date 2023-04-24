3ded3739d6a6a9316b3284668966bb275077c06d
Room 404 <dbuhanov366@gmail.com>
2023-04-24 23:03:10 +0900

    Update bild 001.py

--- Сборки/Сборка 13_04_23/bilds/bild 001.py
+++ Сборки/Сборка 13_04_23/bilds/bild 001.py
@@ -3,17 +3,21 @@
 
 
 from pytube import YouTube
+import pytube.query
+from pytube.cli import on_progress
 # https://www.youtube.com/shorts/96LQhbSIFWI
-yt = YouTube('https://www.youtube.com/shorts/96LQhbSIFWI') #ссылка на видео.
+yt = YouTube('https://www.youtube.com/watch?v=4n3zXfkAVZg') #ссылка на видео.
 # yt.stream показывает какое видео ты можешь скачать 
 # (mp4(720) + audio или только mp4(1080) без звука). 
 # Сейчас стоит фильтр по mp4.
 # print(type(yt.streams))
-for i in yt.streams.filter(type="audio"):
+for i in yt.streams.filter(file_extension="mp4"):
     print(i)
+    i: pytube.query.StreamQuery
+    print(i.audio_codec)
 
 
 #print(yt.streams.filter(file_extension='mp4')) 
-stream = yt.streams.get_by_itag(140) #выбираем по тегу, в каком формате будем скачивать.
+#stream = yt.streams.get_by_itag(140) #выбираем по тегу, в каком формате будем скачивать.
 #print(stream)
-stream.download() #загружаем видео.
+#stream.download() #загружаем видео.
